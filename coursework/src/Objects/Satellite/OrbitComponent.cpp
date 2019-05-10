2853b9240f68763acb75933efd02b1971fb2f232
BossOfTheGym <vladimir.deikun1999@gmail.com>
2019-05-10 23:22:16 +0300

    refactor

--- coursework/src/Objects/Satellite/OrbitComponent.cpp
+++ coursework/src/Objects/Satellite/OrbitComponent.cpp
@@ -64,9 +64,9 @@ void OrbitComponent::setOrbit(double h, double e, double i, double Omega, double
 
 	Mat3 Qxx = transpose(
 		Mat3(
-			  cosO * coso - sinO * sino * cosi, -cosO * sino - sinO * cosi * coso, sinO * sini
+			cosO * coso - sinO * sino * cosi, -cosO * sino - sinO * cosi * coso, sinO * sini
 			, sinO * coso + cosO * cosi * sino, -sinO * sino + cosO * cosi * coso, -cosO * sini
-			,        sini * sino              ,         sini * coso              ,      cosi
+			, sini * sino, sini * coso, cosi
 		)
 	);
 
@@ -115,7 +115,7 @@ void OrbitComponent::updateOrbit()
 	using glm::length;
 
 
-	auto planet    = mPlanetPhys.lock();
+	auto planet = mPlanetPhys.lock();
 	auto satellite = mSatellitePhys.lock();
 	if (!planet || !satellite)
 	{
@@ -144,8 +144,8 @@ void OrbitComponent::updateOrbit()
 
 	//4, 5
 	mCv = cross(rv, vv);//
-	mC  = length(mCv);	
-	mP  = mC * mC / mMu;
+	mC = length(mCv);
+	mP = mC * mC / mMu;
 
 	//6.
 	mI = acos(mCv.z / mC);
@@ -153,17 +153,17 @@ void OrbitComponent::updateOrbit()
 	//7, 8
 	mNv = cross(Vec3(0.0, 0.0, 1.0), mCv);
 	double N = length(mNv);
-	
+
 	//9.
 	mRA = acos(mNv.x / N);
-	if(mNv.y <= std::numeric_limits<double>::epsilon())
+	if (mNv.y <= std::numeric_limits<double>::epsilon())
 	{
 		mRA = PI_2 - mRA;
-	}	
+	}
 
 	//10, 11
 	mEv = cross(vv, mCv) / mMu - rv / r;
-	mE  = length(mEv);
+	mE = length(mEv);
 
 	Vec3 uev = (mE > std::numeric_limits<double>::epsilon() ? mEv / mE : Vec3(0.0));
 
@@ -176,10 +176,10 @@ void OrbitComponent::updateOrbit()
 
 	//13.
 	mTA = acos(dot(rv / r, uev));
-	if(vr <= std::numeric_limits<double>::epsilon())
+	if (vr <= std::numeric_limits<double>::epsilon())
 	{
 		mTA = PI_2 - mTA;
-	}	
+	}
 
 	updateSpecificParams();
 }
@@ -188,7 +188,7 @@ void OrbitComponent::updateSpecificParams()
 {
 	if (mE < 1.0) //elliptic
 	{
-		mA  = mP / (1.0 - mE * mE);
+		mA = mP / (1.0 - mE * mE);
 		mEA = 2.0 * atan(sqrt((1.0 - mE) / (1.0 + mE)) * tan(mTA / 2.0));
 		if (mTA > PI)
 		{
@@ -197,17 +197,17 @@ void OrbitComponent::updateSpecificParams()
 
 		mN = sqrt(mMu) / pow(mA, 1.5);
 
-		mT  = 1.0 / mN * (mEA - mE * sin(mEA));
+		mT = 1.0 / mN * (mEA - mE * sin(mEA));
 		mOP = 1.0 / mN * PI_2;
 	}
 	else if (mE > 1.0) // hyperbolic
 	{
-		mA  = mP / (mE * mE - 1.0);
+		mA = mP / (mE * mE - 1.0);
 		mEA = 2 * atanh(sqrt((mE - 1.0) / (mE + 1.0)) * tanh(mTA / 2));
 
 		mN = sqrt(mMu) / pow(mA, 1.5);
 
-		mT  = 1.0 / mN * (mE * sinh(mEA) - mEA); //something strange happens here
+		mT = 1.0 / mN * (mE * sinh(mEA) - mEA); //something strange happens here
 		mOP = std::numeric_limits<double>::infinity();
 	}
 	else
@@ -218,3 +218,190 @@ void OrbitComponent::updateSpecificParams()
 		mEA = 0.0;
 	}
 }
+
+
+//#include "OrbitComponent.h"
+//
+//#include <limits>
+//
+//#include <Numerics/Equ/Neuton.h>
+//
+//
+//OrbitComponent::OrbitComponent(IComponent* parent)
+//	: IComponent(parent)
+//
+//	, m_h(0.0f)
+//	, m_i(0.0f)
+//	, m_ra(0.0f)
+//	, m_e(0.0f)
+//	, m_ap(0.0f)
+//	, m_ta(0.0f)
+//
+//	, m_p(0.0f)
+//
+//	, m_op(0.0f)
+//	, m_ea(0.0f)
+//	, m_t(0.0f)
+//{}
+//
+//
+//void OrbitComponent::setOrbitFromParams(double mu, double h, double e, double i, double Omega, double omega, double theta)
+//{
+//	using glm::transpose;
+//	using glm::cross;
+//	using glm::length;
+//
+//
+//	double cost = cos(theta);
+//	double sint = sin(theta);
+//
+//	double coso = cos(omega);
+//	double sino = sin(omega);
+//	double cosO = cos(Omega);
+//	double sinO = sin(Omega);
+//	double cosi = cos(i);
+//	double sini = sin(i);
+//
+//	Vec3 rx = h * h / mu / (1.0 + e * cost) * Vec3(cost, sint, 0.0);
+//	Vec3 vx = mu / h * Vec3(-sint, e + cost, 0.0);
+//
+//	Mat3 Qxx = transpose(
+//		Mat3(
+//			  cosO * coso - sinO * sino * cosi, -cosO * sino - sinO * cosi * coso, sinO * sini
+//			, sinO * coso + cosO * cosi * sino, -sinO * sino + cosO * cosi * coso, -cosO * sini
+//			,        sini * sino              ,         sini * coso              ,      cosi
+//		)
+//	);
+//
+//	//setting parameters
+//	m_r = Qxx * rx;
+//	m_v = Qxx * vx;
+//
+//	m_h = h;
+//	m_en = h;
+//	m_e = e;
+//	m_i = i;
+//	m_p = h * h / mu;
+//	m_ap = omega;
+//	m_ra = Omega;
+//	m_ta = theta;
+//
+//	m_hv = cross(m_r, m_v);
+//	m_nv = cross(Vec3(0.0, 0.0, 1.0), m_hv);
+//	m_ev = cross(m_v, m_hv) / mu - m_r / length(m_r);
+//
+//	updateSpecificParams(mu);
+//}
+//
+////TODO
+//OrbitComponent::R_V OrbitComponent::orbitStateAngle(double theta) const
+//{
+//	//TODO
+//
+//	return {};
+//}
+//
+//OrbitComponent::R_V OrbitComponent::orbitStateTime(const Time& dt) const
+//{
+//	//TODO
+//
+//	return {};
+//}
+//
+//
+//void OrbitComponent::setOrbitFromStateVec(double mu, const Vec3& center, const Vec3& rVec, const Vec3& vVec)
+//{
+//	using glm::cross;
+//	using glm::dot;
+//	using glm::length;
+//
+//	//algorithm
+//
+//	//1.
+//	Vec3  rv = rVec - center;
+//	double r = length(rv);
+//
+//	//2.
+//	Vec3  vv = vVec;
+//	double v = length(vVec);
+//	m_en = dot(vv, vv) - 2.0 * mu / r;
+//
+//	//3.
+//	double vr = dot(rv / r, vv);
+//
+//	//4, 5
+//	m_hv = cross(rv, vv);//
+//	m_h  = length(m_hv);	
+//	m_p  = m_h * m_h / mu;
+//
+//	//6.
+//	m_i = acos(m_hv.z / m_h);
+//
+//	//7, 8
+//	m_nv = cross(Vec3(0.0, 0.0, 1.0), m_hv);
+//	double N = length(m_nv);
+//	
+//	//9.
+//	m_ra = acos(m_nv.x / N);
+//	if(m_nv.y <= std::numeric_limits<double>::epsilon())
+//	{
+//		m_ra = PI_2 - m_ra;
+//	}	
+//
+//	//10, 11
+//	m_ev = cross(vv, m_hv) / mu - rv / r;
+//	m_e  = length(m_ev);
+//
+//	Vec3 uev = (m_e > std::numeric_limits<double>::epsilon() ? m_ev / m_e : Vec3(0.0));
+//
+//	//12.
+//	m_ap = acos(dot((m_nv / N), uev));
+//	if (m_ev.z <= std::numeric_limits<double>::epsilon())
+//	{
+//		m_ap = PI_2 - m_ap;
+//	}
+//
+//	//13.
+//	m_ta = acos(dot(rv / r, uev));
+//	if(vr <= std::numeric_limits<double>::epsilon())
+//	{
+//		m_ta = PI_2 - m_ta;
+//	}	
+//
+//	updateSpecificParams(mu);
+//}
+//
+//void OrbitComponent::updateSpecificParams(double mu)
+//{
+//	if (m_e < 1.0) //elliptic
+//	{
+//		m_a  = m_p / (1.0 - m_e * m_e);
+//		m_ea = 2.0 * atan(sqrt((1.0 - m_e) / (1.0 + m_e)) * tan(m_ta / 2.0));
+//		if (m_ta > PI)
+//		{
+//			m_ea = PI_2 + m_ea;
+//		}
+//
+//		m_n = sqrt(mu) / pow(m_a, 1.5);
+//
+//		m_t  = 1.0 / m_n * (m_ea - m_e * sin(m_ea));
+//		m_op = 1.0 / m_n * PI_2;
+//	}
+//	else if (m_e > 1.0) // hyperbolic
+//	{
+//		m_a  = m_p / (m_e * m_e - 1.0);
+//		m_ea = 2 * atanh(sqrt((m_e - 1.0) / (m_e + 1.0)) * tanh(m_ta / 2));
+//
+//		m_n = sqrt(mu) / pow(m_a, 1.5);
+//
+//		m_t  = 1.0 / m_n * (m_e * sinh(m_ea) - m_ea); //something strange happens here
+//		m_op = std::numeric_limits<double>::infinity();
+//	}
+//	else
+//	{
+//		m_a = 0.0;
+//		m_t = 0.0;
+//		m_op = 0.0;
+//		m_ea = 0.0;
+//	}
+//}
--- coursework/src/Objects/Satellite/OrbitComponent.h
+++ coursework/src/Objects/Satellite/OrbitComponent.h
@@ -18,18 +18,18 @@ public:
 
 public:
 	OrbitComponent(
-		  IComponent* parent
+		IComponent* parent
 		, const PhysicsComponentWeak& planet
 		, const PhysicsComponentWeak& satellite
 	);
 
 	OrbitComponent(const OrbitComponent& comp) = default;
-	OrbitComponent(OrbitComponent&& comp)      = default;
+	OrbitComponent(OrbitComponent&& comp) = default;
 
 	virtual ~OrbitComponent() = default;
 
 	OrbitComponent& operator = (const OrbitComponent& comp) = default;
-	OrbitComponent& operator = (OrbitComponent&& comp)      = default;
+	OrbitComponent& operator = (OrbitComponent&& comp) = default;
 
 
 public:
@@ -112,7 +112,7 @@ public:
 	}
 
 
-			
+
 	double period() const
 	{
 		return mOP;
@@ -150,7 +150,7 @@ private:
 	double mAP;  // Argument of Periapsis
 	double mTA;  // True Anomaly
 
-	//misc
+				 //misc
 	Vec3 mCv;   // specific Angular Momentum
 	Vec3 mEv;   // apsis line
 	Vec3 mNv;   // node line
@@ -159,7 +159,7 @@ private:
 	double mP;   // semi-latus rectum
 	double mMu;  // mu param
 
-	//specific
+				 //specific
 	double mN;   // mean motion
 	double mOP;  // Orbit Period
 	double mA;   // apoapsis
@@ -170,4 +170,170 @@ private:
 
 //aliases
 using OrbitComponentShared = std::shared_ptr<OrbitComponent>;
-using OrbitComponentWeak   = std::weak_ptr<OrbitComponent>;
+using OrbitComponentWeak = std::weak_ptr<OrbitComponent>;
+
+
+//#pragma once
+//
+//#include <Common.h>
+//
+//#include "../IObject.h"
+//
+//#include <Physics/PhysicsComponent.h>
+//
+//
+//struct OrbitComponent : public IComponent
+//{
+//public:
+//	struct R_V
+//	{
+//		Vec3 radius;
+//		Vec3 velocity;
+//	};
+//
+//public:
+//	OrbitComponent(IComponent* parent);
+//
+//	OrbitComponent(const OrbitComponent& comp) = default;
+//	OrbitComponent(OrbitComponent&& comp)      = default;
+//
+//	virtual ~OrbitComponent() = default;
+//
+//	OrbitComponent& operator = (const OrbitComponent& comp) = default;
+//	OrbitComponent& operator = (OrbitComponent&& comp)      = default;
+//
+//
+//public:
+//	R_V orbitStateAngle(double theta) const;
+//
+//	R_V orbitStateTime(const Time& dt) const;
+//
+//
+//	void setOrbitFromStateVec(double mu, const Vec3& center, const Vec3& r, const Vec3& v);
+//
+//	void setOrbitFromParams(double mu, double h, double e, double i, double Omega, double omega, double thetha);
+//
+//
+//	//get
+//	double angularMomentum() const
+//	{
+//		return m_h;
+//	}
+//
+//	double inclination() const
+//	{
+//		return m_i;
+//	}
+//
+//	double rightAscension() const
+//	{
+//		return m_ra;
+//	}
+//
+//	double eccentricity() const
+//	{
+//		return m_e;
+//	}
+//
+//	double argumentOfPeriapsis() const
+//	{
+//		return m_ap;
+//	}
+//
+//	double trueAnomaly() const
+//	{
+//		return m_ta;
+//	}
+//
+//
+//	Vec3 angularMomentumV() const
+//	{
+//		return m_hv;
+//	}
+//
+//	Vec3 apsisLineV() const
+//	{
+//		return m_ev;
+//	}
+//
+//	Vec3 nodeLineV() const
+//	{
+//		return m_nv;
+//	}
+//
+//
+//	double energy() const
+//	{
+//		return m_en;
+//	}
+//
+//	double semi_latusRectum() const
+//	{
+//		return m_p;
+//	}
+//
+//	double meanMotion() const
+//	{
+//		return m_n;
+//	}
+//
+//
+//			
+//	double period() const
+//	{
+//		return m_op;
+//	}
+//
+//	double apoapsis() const
+//	{
+//		return m_a;
+//	}
+//
+//	double eccentricAnomaly() const
+//	{
+//		return m_ea;
+//	}
+//
+//	double time() const
+//	{
+//		return m_t;
+//	}
+//
+//
+//private:
+//	void updateSpecificParams(double mu);
+//
+//
+//private:
+//	//main
+//	double m_h;   // specific Angular Momentum(length)
+//	double m_i;   // Inclination
+//	double m_ra;  // right ascension of the Ascending Node
+//	double m_e;   // Eccentricity
+//	double m_ap;  // Argument of Periapsis
+//	double m_ta;  // True Anomaly
+//
+//	//misc
+//	Vec3 m_hv;   // specific Angular Momentum
+//	Vec3 m_ev;   // apsis line
+//	Vec3 m_nv;   // node line
+//
+//	double m_en;   // energy integral
+//	double m_p;   // semi-latus rectum
+//
+//	//specific
+//	double m_n;   // mean motion
+//	double m_op;  // Orbit Period
+//	double m_a;   // apoapsis
+//	double m_ea;  // Eccentric Anomaly
+//	double m_t;   // time since periapsis	
+//
+//	//state vec
+//	Vec3 m_r;
+//	Vec3 m_v;
+//};
+//
+//
+////aliases
+//using OrbitComponentShared = std::shared_ptr<OrbitComponent>;
+//using OrbitComponentWeak   = std::weak_ptr<OrbitComponent>;
